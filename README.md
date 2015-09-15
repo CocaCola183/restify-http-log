0e3ad90f70c94b8db865412e5842058fb0176b89
徐基伟 <1072838309@qq.com>
2015-09-15 11:04:07 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # restify-http-log
-Restify http log for self use.
+Restify http log plugins.
+
+## Before you start
+* restify ^4.0.2  
+* Node 0.12
+
+## Install
+```
+npm install restify-http-log  
+```
+
+## Default usage
+```js
+var http_log = require('restify-http-log');  
+server.pre(http_log());  
+```
+Note that it is better to use `pre` than `use`    
+
+## Configuration
+Default configuration:  
+```js
+{
+	log_dir: process.cwd() + "/log/",
+	http_log_dir: 'http',
+	color: 'white'
+}
+```
+explain:  
+* log_dir: where you store your log files  
+* http_log_dir: the folder you store your http log files in the log folder  
+* color: the http log color output in console, see [this](https://www.npmjs.com/package/colors)  
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-var logger = require('color-journal')({
-	"log_dir": process.cwd() + "/log/",
-	"record_level": 0,
-	"log_methods": {
-	  "http": { "color": "white", "rank": 0}
-	}
-});
+var assert = require('assert-plus');
 
-module.exports = function (req, res, next) {
-  var time_start = Date.now();
-  var _send = res.send;
-  res.send = function() {
-    res.header('X-Execution-Time', String(Date.now() - time_start));
-    logger.http(req.method, req.url, req.headers.host, req.headers['user-agent'], String(Date.now() - time_start), 'ms');
-    return _send.apply(res, arguments);
+module.exports = function restify_http_log (opts) {
+  opts = opts || {};
+  opts.log_dir = opts.log_dir || process.cwd() + "/log/";
+  opts.http_log_dir = opts.http_log_dir || 'http';
+  opts.color = opts.color || 'white';
+  var logger_opts = {
+    "log_dir": opts.log_dir,
+    "record_level": 0
+  };
+  logger_opts["log_methods"] = {};
+  logger_opts["log_methods"][opts.http_log_dir] = {
+    color: opts.color,
+    rank: 0
   }
-  next();
-}
+  
+  var logger = require('color-journal')(logger_opts);
 
+  return function (req, res, next) {
+    var time_start = Date.now();
+    var _send = res.send;
+    res.send = function() {
+      res.header('X-Execution-Time', String(Date.now() - time_start));
+      logger.http(req.method, req.url, req.headers.host, req.headers['user-agent'], String(Date.now() - time_start), 'ms');
+      return _send.apply(res, arguments);
+    }
+    next();
+  };
+}
